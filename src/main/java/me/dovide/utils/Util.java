750b00520a8139aacab5d9a00bf3f6df2ee32347
Dovide <davidelao30@gmail.com>
2024-06-25 15:04:05 +0200

    Fixed Imports

--- src/main/java/me/dovide/utils/Util.java
+++ src/main/java/me/dovide/utils/Util.java
@@ -2,6 +2,8 @@ package me.dovide.utils;
 
 import net.md_5.bungee.api.ChatColor;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
